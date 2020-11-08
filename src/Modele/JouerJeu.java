efe283a61d05fcfab77116c7fb7e361b68949ff6
codingTheWorld777 <khainguyenhuu121@gmail.com>
2020-11-08 17:58:04 +0100

    Update changes

--- src/Modele/JouerJeu.java
+++ src/Modele/JouerJeu.java
@@ -2,13 +2,6 @@ package Modele;
 
 public class JouerJeu {	
 	public static void main(String[] args) {
-		System.out.println("Commencer à jouer!");
 		
-		PiocheCartes piocheCartes = new PiocheCartes();
-		
-		piocheCartes.creerCartes();
-		
-		
-		JoueurPhy joueurPhy = new JoueurPhy("JoueurPhy", 1);
 	}
 }
