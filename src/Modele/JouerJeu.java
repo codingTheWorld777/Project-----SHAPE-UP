c1ae43a7efcf77c291baf504288fb2bfbf2873ac
codingTheWorld777 <khainguyenhuu121@gmail.com>
2020-11-16 12:39:40 +0100

    Update JouerJeu

--- src/Modele/JouerJeu.java
+++ src/Modele/JouerJeu.java
@@ -3,7 +3,8 @@ package Modele;
 public class JouerJeu {	
 	public static void main(String[] args) {
 		Partie partie = new Partie();
-		Compteur compteurPoint = new Compteur(partie);
+		
+		Compteur compteurPoint = new Compteur(Partie.getTableDuJeu());
 		
 		for (int i = 0; i < Partie.joueursEnJeu.length; i++) {
 			System.out.print(Partie.joueursEnJeu[i].nom 
